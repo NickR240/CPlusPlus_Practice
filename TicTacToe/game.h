d07180ff392f373f65c764d3b792da89c15f3ed8
NicholasR240 <nickr240@hotmail.com>
2026-06-17 08:28:06 -0500

    Add game.h header file for TicTacToe

---
+++ TicTacToe/game.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+void initGame();
+void displayWelcomeMessage();
+void getPlayerNames();
+void displayBoard();
+void playerSelect(int choice, int board[], char player);
